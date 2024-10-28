f524c45b6f5a60846a54ceeba621fd85ebf26edd
Ji__ho <cale.jiho@gmail.com>
2024-10-28 19:48:21 +0900

    Env: 폰트 설치 + shadcn 설치

---
+++ public/fonts/fonts.ts
@@ -0,0 +1,11 @@
+import { Sen } from 'next/font/google'
+import localFont from 'next/font/local'
+
+// export const mada = Mada({ weight: '700', subsets: ['latin'], variable: '--font-mada' })
+export const sen = Sen({ weight: '400', subsets: ['latin'], variable: '--font-sen' })
+
+export const pretendard = localFont({
+  src: './PretendardVariable.woff2',
+  weight: '45 920',
+  variable: '--font-pretendard',
+})
