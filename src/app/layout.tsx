7902756e3ad693f708540235b918dafddf1d09d2
Ji__ho <cale.jiho@gmail.com>
2024-11-18 16:27:42 +0900

    시도 pwa splash screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,10 +11,19 @@ export const metadata: Metadata = {
   title: 'Sokk',
   description: '성균관대 지능형 소프트웨어학과 라운지 좌석 시스템',
   viewport: 'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
-  icons: {
-    icon: '../app/favicon.ico',
-  },
   manifest: '/manifest.json',
+  icons: {
+    icon: '../../public/images/splash.png',
+  },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black-translucent',
+    startupImage: [
+      {
+        url: '/images/splash.png',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
